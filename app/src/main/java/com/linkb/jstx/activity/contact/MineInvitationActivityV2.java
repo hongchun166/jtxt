da2442a11a13deb6349635ee03a39b455eda68c5
pyb <zhuizhu_1@xina.com>
2019-12-30 22:38:02 +0800

    1，提交测试11

--- app/src/main/java/com/linkb/jstx/activity/contact/MineInvitationActivityV2.java
+++ app/src/main/java/com/linkb/jstx/activity/contact/MineInvitationActivityV2.java
@@ -6,7 +6,7 @@ import com.linkb.jstx.activity.base.BaseActivity;
 public class MineInvitationActivityV2 extends BaseActivity {
     @Override
     protected void initComponents() {
-        System.out.println("测试代码提交");
+        System.out.println("测试代码提交kkk");
     }
 
     @Override
