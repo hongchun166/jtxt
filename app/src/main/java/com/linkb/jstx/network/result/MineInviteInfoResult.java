5c2881543af4276a341e3a113d031d77ca8c28d8
hongchun166 <756774773@qq.com>
2020-01-07 08:32:15 +0800

    Update MineInviteInfoResult.java
    字段修改

--- app/src/main/java/com/linkb/jstx/network/result/MineInviteInfoResult.java
+++ app/src/main/java/com/linkb/jstx/network/result/MineInviteInfoResult.java
@@ -4,9 +4,9 @@ public class MineInviteInfoResult extends BaseResult {
     public InviteInfo data;
 
     public class InviteInfo {
-        public String inviteCount;   //直接邀请人数
+        public String referrerNumber;   //直接邀请人数
         public String rule;
         public String prize;  //邀请的奖励
-        public String inviteCount2; //间接邀请的人数
+        public String jianjieReferrerNumber; //间接邀请的人数
     }
 }
