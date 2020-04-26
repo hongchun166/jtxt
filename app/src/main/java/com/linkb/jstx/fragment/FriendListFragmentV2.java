f46be8dd79b9f5b545745b65566e134219ad63c4
hongchun166 <756774773@qq.com>
2020-04-26 18:04:25 +0800

    Update FriendListFragmentV2.java

--- app/src/main/java/com/linkb/jstx/fragment/FriendListFragmentV2.java
+++ app/src/main/java/com/linkb/jstx/fragment/FriendListFragmentV2.java
@@ -8,7 +8,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.DefaultItemAnimator;
+import com.linkb.jstx.listener.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
